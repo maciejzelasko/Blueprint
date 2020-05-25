160fb866c28056d6a99c668427c86c03e2c41e36
mzelasko <maciek.z@outlook.com>
2020-05-25 20:17:22 +0100

    Added check to verify if Domain doesnt have dependency to infrastructure

--- src/Blueprint.Domain/Repositories/IWeatherForecastRepo.cs
+++ src/Blueprint.Domain/Repositories/IWeatherForecastRepo.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 using Blueprint.Domain.Entities;
+
+[assembly: InternalsVisibleTo(nameof(Blueprint.Domain) + ".Tests")]
 
 namespace Blueprint.Domain.Repositories
 {
---
+++ test/Blueprint.Domain.Tests/ArchTests.cs
@@ -0,0 +1,26 @@
+﻿using Blueprint.Domain.Repositories;
+using FluentAssertions;
+using NetArchTest.Rules;
+using Xunit;
+
+namespace Blueprint.Domain.Tests
+{
+    public class ArchTests
+    {
+        [Fact]
+        public void BlueprintDomain_DoesNotHaveDependency_ToBlueprintInfrastructure()
+        {
+            // Arrange
+            const string infrastructureNamespace = "Blueprint.Infrastructure";
+
+            // Act
+            var result = Types.InAssembly(typeof(IWeatherForecastRepo).Assembly)
+                .Should()
+                .NotHaveDependencyOnAny(infrastructureNamespace)
+                .GetResult();
+
+            // Assert
+            result.FailingTypes.Should().BeNullOrEmpty();
+        }
+    }
+}
