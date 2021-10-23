cb68eccb045c2b6de4f1c8ade2fba3f9f6f6ac78
maciejzelasko <maciek.z@outlook.com>
2021-10-23 19:47:13 +0200

    Added example value objects

---
+++ src/Blueprint.Domain/ValueObjects/Address.cs
@@ -0,0 +1,16 @@
+﻿namespace Blueprint.Domain.ValueObjects
+{
+    public record Address
+    {
+        private Address(string street, string zipCode)
+        {
+            Street = street;
+            ZipCode = zipCode;
+        }
+
+        public string Street { get; }
+        public string ZipCode { get; }
+
+        public static Address Create(string street, string zipCode) => new (street, zipCode);
+    }
+}
---
+++ test/Blueprint.Domain.Tests/ValueObjects/AddressTests.cs
@@ -0,0 +1,25 @@
+﻿using Blueprint.Domain.ValueObjects;
+using FluentAssertions;
+using Xunit;
+
+namespace Blueprint.Domain.Tests.ValueObjects
+{
+    public class AddressTests
+    {
+        [Fact]
+        public void SameAddressesAreEqual()
+        {
+            // Arrange
+            var address1 = Address.Create("Krakowska", "38-100");
+            var address2 = Address.Create("Krakowska", "38-100");
+
+            // Act
+            var address1String = address1.ToString();
+            var address2String = address2.ToString();
+
+            // Act & Assert
+            address1.Should().Be(address2);
+            address1String.Should().Be(address2String);
+        }
+    }
+}
