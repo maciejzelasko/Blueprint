8bd4692e65ca5d0e8f9d8677778bb385cab17257
maciejzelasko <maciek.z@outlook.com>
2021-11-06 17:14:26 +0100

    Removed startup and merged with program

--- src/Blueprint.Api/Program.cs
+++ src/Blueprint.Api/Program.cs
@@ -1,7 +1,34 @@
-using Blueprint.Api;
+using Blueprint.App.DI;
+using Blueprint.Infrastructure.DI;
+using Microsoft.OpenApi.Models;
 
-CreateHostBuilder(args).Build().Run();
+var builder = WebApplication.CreateBuilder(args);
 
-static IHostBuilder CreateHostBuilder(string[] args) =>
-    Host.CreateDefaultBuilder(args)
-        .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(o =>
+{
+    o.SwaggerDoc("v1", new OpenApiInfo { Title = "api", Version = "v1" });
+    o.DescribeAllParametersInCamelCase();
+});
+
+builder.Services.AddInfrastructure();
+builder.Services.AddBlueprintApp();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
